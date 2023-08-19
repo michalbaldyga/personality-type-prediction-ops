c542b03e05431bb2661fc929f2faed22ec59e1a1
michalbaldyga <michal.baldyga01@gmail.com>
2023-08-19 13:34:07 +0200

    PTP-9 - add model testing

---
+++ backend/test.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+
+from predict import predict
+from tqdm import tqdm
+
+
+def _get_path_to_model() -> str:
+    path = os.path.join(os.path.dirname(__file__), 'model')
+    return path if os.path.isdir(path) else os.path.join(os.path.dirname(__file__), 'release', 'model')
+
+
+def test_model() -> float:
+    """Calculate model accuracy on the test dataset."""
+
+    # Read test dataset
+    df = pd.read_csv('../datasets/tweets_test.csv', delimiter='|')
+    df = df.dropna()  # Remove all None rows
+
+    # Initialize accuracy variables
+    correct, total = 0, len(df['text'])
+
+    # Iterate over each example in the test dataset
+    for i in tqdm(range(total)):
+        # Get text and label for current example
+        text = str(df['text'][i])
+        label = df['label'][i]
+
+        # Make a prediction using the classifier
+        guess = predict(text)[0]['label']
+
+        # Check if the predicted label matches the true label
+        correct_letters = 0
+        if guess == label:
+            correct_letters += 1
+
+    # Calculate accuracy as the ratio of correct predictions to total examples
+    accuracy = correct / total
+    return accuracy
+
+
+print(f"Accuracy: {test_model()}")
