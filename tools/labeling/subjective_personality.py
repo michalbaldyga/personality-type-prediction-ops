172e0f38ca61188f20b97a3240a65783df56d094
michalbaldyga <michal.baldyga01@gmail.com>
2023-10-03 22:58:31 +0200

    PTP-14 - initial scraping with selenium

--- tools/labeling/subjective_personality.py
+++ tools/labeling/subjective_personality.py
@@ -1,9 +1,23 @@
 """ Class used to get persons from subjectivepersonality.com with ops type and link to interview """
+from typing import Tuple
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 ELEMENTS_ON_PAGE = 50
+
+
+def get_image_url(text: str) -> str:
+    tag = '&quot;'
+    start = text.find(tag)
+    end = text.find(tag, start+1)
+    result = text[start+len(tag):end]
+    return result
+
+
+def get_name_and_ops(text: str) -> Tuple[str, str]:
+    result = text.split('\n')
+    return result[0], result[1]
 
 
 class SubjectivePersonality:
@@ -18,26 +32,31 @@ class SubjectivePersonality:
         :return: list of persons: [(ops type, link to interview)]
         """
         results = []
+        show_types_button = self.driver.find_element(By.CLASS_NAME, 'btn-sm')
+        show_types_button.click()
 
         while len(results) < limit:
-            page_limit = min(ELEMENTS_ON_PAGE, limit - len(results))
-            elements = self.__get_elements_from_page(page_limit)
+            elements_with_text = self.__get_elements_from_page('type-record')
+            elements_with_images = self.__get_elements_from_page('image-img')
 
-            for element in elements:
-                ops_type = self.__get_ops_type(element)
-                link = self.__get_interview_link(element)
-                results.append((ops_type, link))
+            # we need to skip every second element
+            elements_with_images = elements_with_images[::2]
+
+            for text, image in zip(elements_with_text, elements_with_images):
+                image_url = get_image_url(image.get_attribute("outerHTML"))
+                name, ops = get_name_and_ops(text.text)
+                results.append({'name': name, 'ops': ops, 'image_url': image_url})
 
             self.__go_to_next_page()
 
         return results
 
-    def __get_elements_from_page(self, limit) -> list:
-        """ Get all elements with person from current page
-        :param limit: maximum number of elements to get
+    def __get_elements_from_page(self, class_name: str) -> list:
+        """ Get class elements from current page
+        :param class_name: class name
         :return: list of elements
         """
-        pass
+        return self.driver.find_elements(By.CLASS_NAME, class_name)
 
     def __go_to_next_page(self):
         """ Click "Next" button to go to next page
@@ -58,3 +77,7 @@ class SubjectivePersonality:
         :return: extracted link to interview
         """
         pass
+
+
+sp = SubjectivePersonality()
+sp.get_persons(1900)
