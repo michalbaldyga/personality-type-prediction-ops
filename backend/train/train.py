c25817fad7db5a479c4da0003c0b3b17a5ee150b
michalbaldyga <michal.baldyga01@gmail.com>
2023-11-05 12:33:22 +0100

    PTP-26 - decrease num_train_epochs

--- backend/train/train.py
+++ backend/train/train.py
@@ -82,7 +82,7 @@ for (coin, idx) in zip(COINS, range(0, COINS_NUMBER)):
         learning_rate=2e-5,
         per_device_train_batch_size=16,
         per_device_eval_batch_size=16,
-        num_train_epochs=5,
+        num_train_epochs=2,
         weight_decay=0.01,
         evaluation_strategy="epoch",
         save_strategy="epoch",
