80d539064bc4df60b216e66180be01383c47a117
jakubantoszek <jakubantoszek1@gmail.com>
2023-11-18 18:23:35 +0100

    [PTP-41] filter videos by name and surname in title

--- backend/gathering_data/YoutubeBrowser.py
+++ backend/gathering_data/YoutubeBrowser.py
@@ -4,24 +4,24 @@ pip install --upgrade google-api-python-client
 pip install --upgrade google-auth-oauthlib google-auth-httplib2
 """
 
-from googleapiclient.discovery import build
 import re
 import requests
 
-API_KEY = 'AIzaSyAXSg_4raL8Mzi0p4y31rQdeJzdEBVZw2E'
 DURATION_LIMIT = 30
 
 
 class YoutubeBrowser:
     def __init__(self):
-        self.youtube = build('youtube', 'v3', developerKey=API_KEY)
         self.youtube_url = 'https://www.youtube.com/'
         self.watch_url = f'{self.youtube_url}watch?v='
         self.search_url = f'{self.youtube_url}results?search_query='
         self.length_filter = '&sp=EgIYAg%253D%253D'
 
-    def get_interview(self, person):
+    def get_interview_by_person(self, person):
         page_url = f"{self.search_url}{'+'.join(person.split())}+interview{self.length_filter}"
+        return self.get_interview_by_search_link(page_url, person)
+
+    def get_interview_by_search_link(self, page_url, person):
         page = requests.get(page_url)
         content = page.content.decode()
         urls = re.findall('/watch\?v=[^"\s]+', content)
@@ -31,27 +31,37 @@ class YoutubeBrowser:
             u = url.split("\\")
             unique_urls.add(u[0])
 
-        for video_id in unique_urls:
+        for video_id in list(unique_urls)[:20]:
             video_url = f"{self.youtube_url}{video_id}"
             if self.__is_interview_proper(video_url, person):
                 return video_url
+
+        return None
 
     def __is_interview_proper(self, video_url, person):
         page = requests.get(video_url)
         content = page.content.decode()
 
         title = self.__get_video_title(content)
-        duration = self.__get_video_duration(content)
 
-        if person in title and duration > DURATION_LIMIT:
-            return True
-        else:
-            return False
+        result = self.__is_name_in_title(person.lower(), title.lower())
+        print(f"{result}: {person} - {title}")
 
-    def __get_video_title(self, content):
+        return result
+
+    @staticmethod
+    def __get_video_title(content):
         title_tag = re.findall('<title>[^<]+</title>', content)
         title = title_tag[0][7:-8]
         return title
+
+    @staticmethod
+    def __is_name_in_title(name, title):
+        parts = name.split()
+        if parts[0] in title and parts[-1] in title:
+            return True
+
+        return False
 
     def __get_video_duration(self, content):
         duration_tag = re.findall('meta itemprop="duration" content="PT[0-9A-Z]+', content)
@@ -70,27 +80,3 @@ class YoutubeBrowser:
         # Calculate the total duration in minutes
         total_minutes = (hours * 60) + minutes
         return total_minutes
-
-
-ytb = YoutubeBrowser()
-with open('../../static/csv/records.csv') as records_file:
-    output = []
-    lines = records_file.readlines()
-
-    for l in lines[1:]:
-        line = l[:-1]
-        person = line.split(',')[0]
-
-        try:
-            interview_link = ytb.get_interview(person)
-            print(f"{len(output)}: {interview_link}")
-            if interview_link:
-                output.append(f'{line},{interview_link}')
-            else:
-                output.append(f'{line},')
-        except:
-            break
-
-    with open('../../static/csv/records_update.csv', 'w') as new_file:
-        print(output)
-        new_file.writelines(output)
---
+++ backend/gathering_data/get_interviews.py
@@ -0,0 +1,62 @@
+import csv
+
+from backend.gathering_data.YoutubeBrowser import YoutubeBrowser
+
+browser = YoutubeBrowser()
+CSV_DIR = '../../static/csv/'
+
+
+def get_interviews(records_file_path):
+    x = 0
+    results = [["name", "ops", "interview_link"]]
+
+    with open(records_file_path) as record_file:
+        lines = record_file.readlines()[1:100]
+
+        for line in lines:
+            x += 1
+            cols = line.split(';') if ';' in line else line.split(',')
+            name = cols[0]
+            is_member = int(cols[4])
+
+            yt_link = cols[5].replace('\n', '')
+
+            interview_link = get_interview_link(name, yt_link, is_member)
+
+            if interview_link is not None:
+                results.append([name, cols[1], interview_link])
+
+            print(f"{x}: {interview_link}")
+
+    with open(f"{CSV_DIR}interview_links.csv", "w", newline='') as output_file:
+        output_file.writelines(f"{';'.join(line)}\n" for line in results[:-1])
+        output_file.write(';'.join(results[-1]))
+
+
+def get_interview_link(name, link, is_member):
+    def __validate_name(person_name):
+        parts = person_name.split()
+
+        if len(parts) < 2:
+            return False
+        if len(parts) == 2 and len(parts[1]) < 2:
+            return False
+        return True
+
+    if link is None or len(link) == 0:
+        if __validate_name(name):
+            path = 'https://www.youtube.com/results?search_query='
+            link = f"{path}{name.replace(' ', '%20')}%20interview"
+        else:
+            return None
+    if 'search_query' in link:
+        if __validate_name(name) and not is_member:
+            return browser.get_interview_by_search_link(link, name)
+    else:
+        return link
+
+    return None
+
+
+if __name__ == '__main__':
+    get_interviews(f"{CSV_DIR}records.csv")
