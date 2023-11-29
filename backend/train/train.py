6a202e12fed8d5d913dd881cbd62086705c65f33
michalbaldyga <michal.baldyga01@gmail.com>
2023-11-29 22:17:37 +0100

    PTP-26 - improve code quality

--- backend/train/train.py
+++ backend/train/train.py
@@ -1,32 +1,32 @@
 import os
 
-from datasets import ClassLabel, Dataset
-from transformers import AutoTokenizer, DataCollatorWithPadding, \
-    AutoModelForSequenceClassification, TrainingArguments, Trainer
 import evaluate
 import numpy as np
 import pandas as pd
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, DataCollatorWithPadding, Trainer, TrainingArguments
+
+from datasets import ClassLabel, Dataset
 
 CSV_DIR = "../../static/csv/"
 CSV_WITH_COINS = os.path.join(CSV_DIR, "records_cleaned_processed.csv")
 CSV_WITH_TRANSCRIPTS = os.path.join(CSV_DIR, "transcripts_cleaned.csv")
-COINS = {'Human Needs_Observer': ["Oe", "Oi"],
-         'Human Needs_Decider': ["De", "Di"],
-         'Human Needs_Preferences': ["DD", "OO"],
-         'Letter_Observer': ["S", "N"],
-         'Letter_Decider': ["F", "T"],
-         'Animal_Energy Animal': ["Play", "Sleep"],
-         'Animal_Info Animal': ["Consume", "Blast"],
-         'Animal_Dominant Animal': ["Info", "Energy"],
-         'Animal_Introverted vs Extraverted': ["Extro", "Intro"],
-         'Sexual Modality_Sensory': ["M", "F"],
-         'Sexual Modality_Extraverted Decider': ["M", "F"]}
+COINS = {"Human Needs_Observer": ["Oe", "Oi"],
+         "Human Needs_Decider": ["De", "Di"],
+         "Human Needs_Preferences": ["DD", "OO"],
+         "Letter_Observer": ["S", "N"],
+         "Letter_Decider": ["F", "T"],
+         "Animal_Energy Animal": ["Play", "Sleep"],
+         "Animal_Info Animal": ["Consume", "Blast"],
+         "Animal_Dominant Animal": ["Info", "Energy"],
+         "Animal_Introverted vs Extraverted": ["Extro", "Intro"],
+         "Sexual Modality_Sensory": ["M", "F"],
+         "Sexual Modality_Extraverted Decider": ["M", "F"]}
 
 
 def preprocess_function(batch):
     """Preprocessing function to tokenize text and sequences."""
-    tokenized_batch = tokenizer(batch['text'], truncation=True)
-    tokenized_batch['label'] = labels.str2int(batch['label'])
+    tokenized_batch = tokenizer(batch["text"], truncation=True)
+    tokenized_batch["label"] = labels.str2int(batch["label"])
     return tokenized_batch
 
 
@@ -37,28 +37,37 @@ def compute_metrics(eval_pred):
     return accuracy.compute(predictions=predictions, references=labels)
 
 
-def preprocess_coin_data(df_coin, coin):
-    df_coin = df_coin.dropna()
-    coin_counts = df_coin[coin].value_counts()
-    difference = coin_counts[COINS[coin][0]] - coin_counts[COINS[coin][1]]
+def preprocess_coins_dataframe(df: pd.DataFrame, column_name: str) -> pd.DataFrame:
+    """Preprocess a DataFrame by balancing the counts of two specified values in a given column.
+
+    :param df: DataFrame, the input DataFrame.
+    :param column_name: str, the name of the column to be processed.
+    :return: DataFrame, the preprocessed DataFrame with balanced counts for the specified values.
+    """
+    df = df.dropna()
+
+    column_counts = df[column_name].value_counts()
+    difference = column_counts[COINS[column_name][0]] - column_counts[COINS[column_name][1]]
+
     if difference > 0:
-        df_cleaned = pd.concat([df_coin[df_coin[coin] == COINS[coin][0]].iloc[:-difference],
-                                df_coin[df_coin[coin] == COINS[coin][1]]])
+        new_df = pd.concat([df[df[column_name] == COINS[column_name][0]].iloc[:-difference],
+                            df[df[column_name] == COINS[column_name][1]]])
     elif difference < 0:
-        df_cleaned = pd.concat([df_coin[df_coin[coin] == COINS[coin][1]].iloc[:difference],
-                                df_coin[df_coin[coin] == COINS[coin][0]]])
+        new_df = pd.concat([df[df[column_name] == COINS[column_name][1]].iloc[:difference],
+                            df[df[column_name] == COINS[column_name][0]]])
     else:
-        df_cleaned = df_coin.copy()
-    return df_cleaned
+        new_df = df.copy()
+
+    return new_df
 
 
 # Load datasets
-df_coins = pd.read_csv(CSV_WITH_COINS, delimiter=',')
-df_transcripts = pd.read_csv(CSV_WITH_TRANSCRIPTS, delimiter='|')
+df_coins = pd.read_csv(CSV_WITH_COINS, delimiter=",")
+df_transcripts = pd.read_csv(CSV_WITH_TRANSCRIPTS, delimiter="|")
 
 for coin in COINS:
     df_coin = df_coins[["name", coin]]
-    df_cleaned = preprocess_coin_data(df_coin, coin)
+    df_cleaned = preprocess_coins_dataframe(df_coin, coin)
 
     merged_df = pd.merge(df_cleaned, df_transcripts, on="name", how="inner")
 
@@ -84,7 +93,7 @@ for coin in COINS:
     # Train
     model_output_dir = f"../release/model_{coin}"
     model = AutoModelForSequenceClassification.from_pretrained(
-        "distilbert-base-uncased", num_labels=2, id2label=id2label, label2id=label2id
+        "distilbert-base-uncased", num_labels=2, id2label=id2label, label2id=label2id,
     )
 
     training_args = TrainingArguments(
@@ -116,7 +125,3 @@ for coin in COINS:
 
     trainer.save_model(model_output_dir)
     print(f"{coin}_model saved")
-
-    print(f"Evaluation for {coin}")
-    results = trainer.evaluate()
-    print(results)
