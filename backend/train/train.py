a4d1e6241acff3539a0cd254d585f5c579fb56fe
michalbaldyga <michal.baldyga01@gmail.com>
2023-11-29 21:24:55 +0100

    PTP-26 - minor fixes

--- backend/train/train.py
+++ backend/train/train.py
@@ -7,11 +7,9 @@ import evaluate
 import numpy as np
 import pandas as pd
 
-COINS_NUMBER = 11
-VALUES_PER_COIN = 2
 CSV_DIR = "../../static/csv/"
 CSV_WITH_COINS = os.path.join(CSV_DIR, "records_cleaned_processed.csv")
-CSV_WITH_TRANSCRIPTS = os.path.join(CSV_DIR, "transcripts.csv")
+CSV_WITH_TRANSCRIPTS = os.path.join(CSV_DIR, "transcripts_cleaned.csv")
 COINS = {'Human Needs_Observer': ["Oe", "Oi"],
          'Human Needs_Decider': ["De", "Di"],
          'Human Needs_Preferences': ["DD", "OO"],
@@ -23,13 +21,6 @@ COINS = {'Human Needs_Observer': ["Oe", "Oi"],
          'Animal_Introverted vs Extraverted': ["Extro", "Intro"],
          'Sexual Modality_Sensory': ["M", "F"],
          'Sexual Modality_Extraverted Decider': ["M", "F"]}
-
-
-def load_dataset_from_csv(csv_path):
-    """Loading dataset from csv file."""
-    df = pd.read_csv(csv_path, delimiter='|')
-    df = pd.DataFrame(df)
-    return Dataset.from_pandas(df)
 
 
 def preprocess_function(batch):
@@ -46,63 +37,41 @@ def compute_metrics(eval_pred):
     return accuracy.compute(predictions=predictions, references=labels)
 
 
-def calculate_coins_indexes(coin_col):
-    coins_indexes = []
-    changes_cnt = 0
-    curr_coin = coin_col[0]
-    for idx, coin in enumerate(coin_col):
-        if coin != curr_coin:
-            curr_coin = coin
-            changes_cnt += 1
-        if changes_cnt == VALUES_PER_COIN:
-            coins_indexes.append(idx)
-            changes_cnt = 0
-    coins_indexes.append(idx + 1)
-    return coins_indexes
+def preprocess_coin_data(df_coin, coin):
+    df_coin = df_coin.dropna()
+    coin_counts = df_coin[coin].value_counts()
+    difference = coin_counts[COINS[coin][0]] - coin_counts[COINS[coin][1]]
+    if difference > 0:
+        df_cleaned = pd.concat([df_coin[df_coin[coin] == COINS[coin][0]].iloc[:-difference],
+                                df_coin[df_coin[coin] == COINS[coin][1]]])
+    elif difference < 0:
+        df_cleaned = pd.concat([df_coin[df_coin[coin] == COINS[coin][1]].iloc[:difference],
+                                df_coin[df_coin[coin] == COINS[coin][0]]])
+    else:
+        df_cleaned = df_coin.copy()
+    return df_cleaned
 
 
-# Load dataset
+# Load datasets
 df_coins = pd.read_csv(CSV_WITH_COINS, delimiter=',')
 df_transcripts = pd.read_csv(CSV_WITH_TRANSCRIPTS, delimiter='|')
 
 for coin in COINS:
-
     df_coin = df_coins[["name", coin]]
-    df_coin.dropna(inplace=True)
+    df_cleaned = preprocess_coin_data(df_coin, coin)
 
-    # Step 1: Count occurrences of each value in the 'coin' column
-    coin_counts = df_coin[coin].value_counts()
-
-    # Step 2: Find the difference in counts between the two classes
-    difference = coin_counts[COINS[coin][0]] - coin_counts[COINS[coin][1]]
-
-    # Step 3: Drop surplus rows from the class with more occurrences
-    if difference > 0:
-        # If 'OO' has more occurrences, drop the last 'difference' rows with 'DD'
-        df_cleaned = pd.concat([df_coin[df_coin[coin] == COINS[coin][0]].iloc[:-difference],
-                                df_coin[df_coin[coin] == COINS[coin][1]]])
-    elif difference < 0:
-        # If 'DD' has more occurrences, drop the last 'difference' rows with 'OO'
-        df_cleaned = pd.concat([df_coin[df_coin[coin] == COINS[coin][1]].iloc[:difference],
-                                df_coin[df_coin[coin] == COINS[coin][0]]])
-    else:
-        # If counts are already equal, no need to drop any rows
-        df_cleaned = df_coin.copy()
-
-    # Łączenie DataFrame'ów na podstawie kolumny "name"
     merged_df = pd.merge(df_cleaned, df_transcripts, on="name", how="inner")
 
-    # Tworzenie słownika
-    result_dict = {"coin": list(merged_df[coin]),
-                   "transcript": list(merged_df["transcript"])}
+    data_dict = {"label": list(merged_df[coin]),
+                 "text": list(merged_df["transcript"])}
 
     # Load current batch and labels
-    dataset_batch = Dataset.from_dict(result_dict)
+    dataset_batch = Dataset.from_dict(data_dict)
     dataset_batch = dataset_batch.train_test_split(test_size=0.2)
 
-    labels = ClassLabel(names=[coin[0], coin[1]])
-    id2label = {0: coin[0], 1: coin[1]}
-    label2id = {coin[0]: 0, coin[1]: 1}
+    labels = ClassLabel(names=[COINS[coin][0], COINS[coin][1]])
+    id2label = {0: COINS[coin][0], 1: COINS[coin][1]}
+    label2id = {COINS[coin][0]: 0, COINS[coin][1]: 1}
 
     # Preprocess
     tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
@@ -144,7 +113,10 @@ for coin in COINS:
     print(f"Start training for {coin}")
     trainer.train()
     print(f"Training done for {coin}")
+
     trainer.save_model(model_output_dir)
     print(f"{coin}_model saved")
-    trainer.evaluate()
-    print(f"Evaluation done for {coin}")
+
+    print(f"Evaluation for {coin}")
+    results = trainer.evaluate()
+    print(results)
