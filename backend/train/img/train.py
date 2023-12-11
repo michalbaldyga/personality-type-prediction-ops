9774ad29bfb4fd6393f1f4c04b4e0d116aa9982c
BaranskiKacper <kbaranski20@gmail.com>
2023-12-11 15:57:04 +0100

    Update parameters in train.py

--- backend/train/img/train.py
+++ backend/train/img/train.py
@@ -4,8 +4,8 @@ import os
 import pandas as pd
 import tensorflow as tf
 from keras.applications import ResNet50
-from keras.callbacks import ModelCheckpoint
-from keras.layers import Dense, GlobalAveragePooling2D
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.layers import Dense, GlobalAveragePooling2D, Dropout
 from keras.models import Model
 from keras.optimizers import Adam
 from keras.preprocessing.image import ImageDataGenerator
@@ -57,31 +57,48 @@ def preprocess_coins_dataframe(df: pd.DataFrame, column_name: str) -> pd.DataFra
     ])
 
 
-# Function to create and compile a model
-def create_model(train_base_model=False):
-    """Creates a deep learning model based on the ResNet50 architecture with modifications for binary classification.
+def calculate_layers_to_freeze(num_samples, total_layers=165):
+    """
+    Calculate the number of layers to freeze based on the number of samples.
 
-    The model uses the ResNet50 as a base model with its top layer excluded.
-    GlobalAveragePooling2D is applied to the output of the base model. Then, a dense layer with 1024 neurons and
-    ReLU activation is added. The final output layer is a dense layer with a single neuron and sigmoid activation
-    for binary classification. The base model layers are set as non-trainable. An RMSprop optimizer wrapped with
-    mixed precision LossScaleOptimizer is used for compiling the model. The model is compiled with binary
-    crossentropy loss and accuracy as the metric.
+    :param num_samples: int, number of samples in the dataset.
+    :param total_layers: int, total number of layers in the base model.
+    :return: int, number of layers to freeze.
+    """
+    if num_samples < 1000:
+        return total_layers - 10
+    elif num_samples <= 1700:
+        return total_layers - 30
 
+
+def create_model(train_base_model=False, num_layers_to_freeze=None):
+    """
+    Creates a deep learning model based on the ResNet50 architecture with modifications for binary classification.
+
+    :param train_base_model: bool, if True, the base model is trained, otherwise, it's set as non-trainable.
+    :param num_layers_to_freeze: int, number of layers from the top to freeze. If None, default freezing logic is applied.
     :return: Model, the compiled Keras model ready for training.
     """
     base_model = ResNet50(weights="imagenet", include_top=False)
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
-    x = Dense(1024, activation="relu")(x)
+    x = Dropout(0.5)(x)
+    x = Dense(512, activation="relu")(x)
     predictions = Dense(1, activation="sigmoid")(x)
     model = Model(inputs=base_model.input, outputs=predictions)
 
     if not train_base_model:
+        if num_layers_to_freeze is not None:
+            for layer in base_model.layers[:-num_layers_to_freeze]:
+                layer.trainable = False
+        else:
+            for layer in base_model.layers[:-10]:  # Default freezing logic
+                layer.trainable = False
+    else:
         for layer in base_model.layers:
-            layer.trainable = False
+            layer.trainable = True
 
-    optimizer = Adam(learning_rate=0.0001 if not train_base_model else 0.00001)
+    optimizer = Adam(learning_rate=0.00005 if not train_base_model else 0.00001)
     model.compile(optimizer=optimizer, loss="binary_crossentropy", metrics=["accuracy"])
 
     return model
@@ -101,12 +118,13 @@ def main():
     """
     train_datagen = ImageDataGenerator(
         rescale=1. / 255,
-        rotation_range=20,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
+        rotation_range=10,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
         horizontal_flip=True,
-        fill_mode="nearest",
+        fill_mode='nearest'
     )
+
     num_of_folds = 5
     fold = KFold(n_splits=num_of_folds, shuffle=True, random_state=42)
     results = []
@@ -129,8 +147,12 @@ def main():
         # Balance the dataset
         balanced_df = preprocess_coins_dataframe(filtered_df_coin, coin)
 
+        num_samples = len(balanced_df)
+        num_layers_to_freeze = calculate_layers_to_freeze(num_samples)
+
         best_accuracy_for_coin = 0
         best_model_for_coin = None
+        model_paths_to_delete = []  # List to keep track of non-best model paths
 
         for fold_counter, (train_index, test_index) in enumerate(fold.split(balanced_df), start=1):
             train_df = balanced_df.iloc[train_index]
@@ -144,7 +166,7 @@ def main():
                 x_col="image_path",
                 y_col=coin,
                 target_size=(224, 224),
-                batch_size=32,
+                batch_size=16,
                 class_mode="binary",
             )
 
@@ -153,58 +175,86 @@ def main():
                 x_col="image_path",
                 y_col=coin,
                 target_size=(224, 224),
-                batch_size=32,
+                batch_size=16,
                 class_mode="binary",
             )
 
-            model = create_model(train_base_model=False)
-            temp_model_path = os.path.join(model_directory, f"temp_model_{coin}.h5")
-            checkpoint = ModelCheckpoint(temp_model_path, monitor="val_accuracy", save_best_only=True, mode="max")
+            # Create and compile the model
+            model = create_model(train_base_model=False, num_layers_to_freeze=num_layers_to_freeze)
 
+            # Define callbacks including ModelCheckpoint
+            checkpoint_path = os.path.join(model_directory, f"best_model_{coin}_fold_{fold_counter}.h5")
+
+            checkpoint = ModelCheckpoint(
+                checkpoint_path,
+                monitor="val_accuracy",
+                save_best_only=True,
+                mode="max")
+
+            early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
             model.fit(
                 train_generator,
-                steps_per_epoch=len(train_df) // 32,
+                steps_per_epoch=len(train_df) // 16,
                 epochs=10,
                 validation_data=valid_generator,
-                validation_steps=len(valid_df) // 32,
-                callbacks=[checkpoint],
+                validation_steps=len(valid_df) // 16,
+                callbacks=[checkpoint, early_stopping],
             )
 
             # Fine-tuning phase
-            model = create_model(train_base_model=True)
-            for layer in model.layers[:165]:
-                layer.trainable = False
-            for layer in model.layers[165:]:
+
+            model = create_model(
+                train_base_model=True,
+                num_layers_to_freeze=num_layers_to_freeze
+            )
+            model.load_weights(checkpoint_path)
+
+            # Unfreeze the top layers of the model
+            for layer in model.layers[:-num_layers_to_freeze]:
                 layer.trainable = True
+
+            # Recompile the model
             model.compile(optimizer=Adam(learning_rate=0.00001), loss="binary_crossentropy", metrics=["accuracy"])
 
+            early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+            # Continue training (fine-tuning)
             model.fit(
                 train_generator,
-                steps_per_epoch=len(train_df) // 32,
+                steps_per_epoch=len(train_df) // 16,
                 epochs=5,  # Fewer epochs for fine-tuning
                 validation_data=valid_generator,
-                validation_steps=len(valid_df) // 32,
-                callbacks=[checkpoint],
+                validation_steps=len(valid_df) // 16,
+                callbacks=[checkpoint, early_stopping],
             )
 
-            best_fold_model = tf.keras.models.load_model(temp_model_path)
-            loss, accuracy = best_fold_model.evaluate(valid_generator, steps=len(valid_df) // 32)
+            # Evaluate the fine-tuned model
+            best_fold_model = tf.keras.models.load_model(checkpoint_path)
+            loss, accuracy = best_fold_model.evaluate(valid_generator, steps=len(valid_df) // 16)
             logging.info(f"Fold {fold_counter} completed for {coin} with accuracy {accuracy}")
 
+            # Save the best model
             if accuracy > best_accuracy_for_coin:
                 best_accuracy_for_coin = accuracy
-                if best_model_for_coin and fold_counter < num_of_folds:
-                    os.remove(best_model_for_coin)
-                best_model_for_coin = temp_model_path
-                logging.info(f"New best model found for {coin} with accuracy: {accuracy}")
-
+                if best_model_for_coin:
+                    model_paths_to_delete.append(best_model_for_coin)  # Add previous best model to delete list
+                best_model_for_coin = checkpoint_path
+            else:
+                model_paths_to_delete.append(checkpoint_path)  # Add non-best model to delete list
+        # Save the best model outside the fold loop
         final_model_path = os.path.join(model_directory, f"model_{coin}.h5")
         if best_model_for_coin and os.path.exists(best_model_for_coin):
             os.rename(best_model_for_coin, final_model_path)
             logging.info(f"Best model for {coin} saved at {final_model_path}")
 
+        # Delete non-best fold models
+        for model_path in model_paths_to_delete:
+            if os.path.exists(model_path):
+                os.remove(model_path)
+                logging.info(f"Removed non-best model {model_path}")
+
         results.append({"Coin": coin, "Accuracy": best_accuracy_for_coin})
 
+        # Save the accuracy results for all coins
     results_df = pd.DataFrame(results)
     results_df.to_csv(model_img_accuracy_dir, index=False)
     logging.info("Training completed for all coins. Results saved.")
